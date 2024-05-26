150eafc8c67c3524e5fec95e71f84b7544f270c8
erigert <erigert.zaimi@gmail.com>
2024-05-26 04:59:37 +0200

    kerkesa 2

---
+++ example_49/src/main/java/com/eazybytes/eazyschool/constants/EazySchoolConstants.java
@@ -0,0 +1,11 @@
+package com.eazybytes.eazyschool.constants;
+
+public interface EazySchoolConstants {
+
+    public static final String OPEN = "Open";
+    public static final String CLOSE = "Close";
+    public static final String STUDENT_ROLE = "STUDENT";
+    public static final String ADMIN_ROLE = "ADMIN";
+
+    public static final String LECTURER_ROLE = "LECTURER";
+}
